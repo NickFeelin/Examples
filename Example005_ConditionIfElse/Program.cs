43607fe7f46aa00aa7cfe59eaf4592235515fb4a
Nick Filin <flyer.fil@list.ru>
2022-08-05 00:56:44 +0700

    add Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "санька")
+{
+    Console.WriteLine("Ура! Это же Санька - моя любимая жена!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
